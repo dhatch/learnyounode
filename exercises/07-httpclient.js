e65924b1fa190c263966f1f76b07ad82defa26f7
David Hatch <dhatch387@gmail.com>
2015-01-24 20:49:31 -0500

    Add solution to exercise 7.

---
+++ exercises/07-httpclient.js
@@ -0,0 +1,20 @@
+// David Hatch
+// 1/24/15
+//
+// learnyounode - Exercise 7 - HTTP Client
+//
+// Write a program that performs an HTTP GET request to a URL provided to you as
+// the first command-line argument. Write the String contents of each "data"
+// event from the response to a new line on the console (stdout).
+
+var http = require('http'),
+    url  = process.argv[2];
+
+http.get(url, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+        console.log(data);
+    }).on('error', function (err) {
+        console.error(err);
+    });
+});
